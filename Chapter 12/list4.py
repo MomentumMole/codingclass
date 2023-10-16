52dfb74846a5fea1bd33ab6d6d10642e49807ba2
MomntumMole <talktoaidan@icloud.com>
2023-10-16 09:16:00 -0700

    wahhhh

--- Chapter 12/list4.py
+++ Chapter 12/list4.py
@@ -3,16 +3,21 @@ def main():
     unsort = [ 9, 8, 3, 4, 7, 1]
     print(biggest(unsort))
 def biggest(y):
-    if y[0] < y[1]:
-        y.pop(0)
-        biggest(y)
-        
-    elif y[0] > y[1]:
-        y.pop(1)
-        biggest(y)
-    else: 
-        len(y) == 1
-        return 0
+    if len(y) > 2:
+        if y[0] < y[1]:
+            y.remove(y[0])
+            biggest(y)
+        elif y[0] > y[1]:
+            y.remove(y[1])
+            biggest(y)
+        else: 
+            len(y) == 1
+            print("Done")
+    else:
+        if y[0] > y[1]:
+            print(y[0])
+        else:
+            print([1])
         
 
 main()
